fef08331a917d7974a5a9b58560d175af65946e8
cjcjon <cjcjon30@gmail.com>
2023-02-06 23:20:22 +0900

    feat: 연습문제 6-2 구현

--- src/main/kotlin/chapter6/Tree.kt
+++ src/main/kotlin/chapter6/Tree.kt
@@ -4,3 +4,9 @@ sealed class Tree<out T>
 object EmptyTree : Tree<Nothing>()
 data class Node<out T>(val value: T, val left: Tree<T>, val right: Tree<T>) : Tree<T>()
 
+fun Tree<Int>.insert(elem: Int): Tree<Int> = when (this) {
+  is EmptyTree -> Node(value = elem, left = EmptyTree, right = EmptyTree)
+  is Node ->
+    if (elem <= value) Node(value = value, left = left.insert(elem), right = right)
+    else Node(value = value, left = left, right = right.insert(elem))
+}
