09957c18f4baa5ae2ef0ecd9bdddeb8d91dabce7
cjcjon <cjcjon30@gmail.com>
2023-02-07 00:55:25 +0900

    feat: 연습문제 6-5 구현

--- src/main/kotlin/chapter6/Tree.kt
+++ src/main/kotlin/chapter6/Tree.kt
@@ -53,100 +53,21 @@ fun Tree<Int>.insertTailrec(elem: Int): Tree<Int> {
   return rebuild(path(this, elem), elem)
 }
 
+fun Tree<Int>.contains(elem: Int): Boolean = when(this) {
+  is EmptyTree -> false
+  is Node ->
+    if (elem == value) true
+    else if (elem < value) left.contains(elem)
+    else right.contains(elem)
+}
+
 fun main() {
-  val tree1 = EmptyTree.insertTailrec(5)
-  require(tree1 == Node(5, EmptyTree, EmptyTree))
+  val tree1 = EmptyTree.insertTailrec(5).insertTailrec(3).insertTailrec(10).insertTailrec(20).insertTailrec(4)
 
-  val tree2 = tree1.insertTailrec(3)
-  require(
-    tree2 ==
-        Node(
-          5,
-          Node(3, EmptyTree, EmptyTree),
-          EmptyTree
-        )
-  )
-
-  val tree3 = tree2.insertTailrec(10)
-  require(
-    tree3 ==
-        Node(
-          5,
-          Node(3, EmptyTree, EmptyTree),
-          Node(10, EmptyTree, EmptyTree)
-        )
-  )
-
-  val tree4 = tree3.insertTailrec(20)
-  require(
-    tree4 ==
-        Node(
-          5,
-          Node(3, EmptyTree, EmptyTree),
-          Node(
-            10,
-            EmptyTree,
-            Node(20, EmptyTree, EmptyTree)
-          )
-        )
-  )
-
-  val tree5 = tree4.insertTailrec(4)
-  require(
-    tree5 ==
-        Node(
-          5,
-          Node(
-            3,
-            EmptyTree,
-            Node(4, EmptyTree, EmptyTree)
-          ),
-          Node(
-            10,
-            EmptyTree,
-            Node(20, EmptyTree, EmptyTree)
-          )
-        )
-  )
-
-  val tree6 = tree5.insertTailrec(2)
-  require(
-    tree6 ==
-        Node(
-          5,
-          Node(
-            3,
-            Node(2, EmptyTree, EmptyTree),
-            Node(4, EmptyTree, EmptyTree)
-          ),
-          Node(
-            10,
-            EmptyTree,
-            Node(20, EmptyTree, EmptyTree)
-          )
-        )
-  )
-
-  val tree7 = tree6.insertTailrec(8)
-  require(
-    tree7 ==
-        Node(
-          5,
-          Node(
-            3,
-            Node(2, EmptyTree, EmptyTree),
-            Node(4, EmptyTree, EmptyTree)
-          ),
-          Node(
-            10,
-            Node(8, EmptyTree, EmptyTree),
-            Node(20, EmptyTree, EmptyTree)
-          )
-        )
-  )
-
-  // 오래 걸리는데 빨리 돌릴거면 LinkedList로 타입을 바꾸면 된다
-  (1..100000).fold(EmptyTree as Tree<Int>) { acc, i ->
-    acc.insertTailrec(i)
-  }
+  require(tree1.contains(5))
+  require(tree1.contains(3))
+  require(tree1.contains(10))
+  require(tree1.contains(20))
+  require(tree1.contains(4))
+  require(!tree1.contains(8))
 }
