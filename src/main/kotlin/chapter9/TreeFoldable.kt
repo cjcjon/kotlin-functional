a9bd8186e9befefdada399a81cd7905117017cd6
cjcjon <cjcjon30@gmail.com>
2023-03-07 00:36:35 +0900

    feat: 연습문제 9-11 구현

---
+++ src/main/kotlin/chapter9/TreeFoldable.kt
@@ -0,0 +1,16 @@
+package chapter9
+
+sealed class Tree<out A> : Foldable<A>
+
+data class TreeNode<A>(val value: A, val forest: FunList<TreeNode<A>> = Nil) : Tree<A>() {
+
+  override fun <B> foldLeft(acc: B, f: (B, A) -> B): B = when (forest) {
+    is Nil -> f(acc, value)
+    is Cons -> f(loop(forest, f, acc), value)
+  }
+
+  private tailrec fun <B> loop(list: FunList<TreeNode<A>>, f: (B, A) -> B, acc: B): B = when (list) {
+    is Nil -> acc
+    is Cons -> loop(list.tail, f, list.head.foldLeft(acc, f))
+  }
+}
